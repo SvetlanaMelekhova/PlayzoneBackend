74e7401e16d7baaee458381066455a821229b5a1
SvetlanaMelekhova <s.melekhova@dinamicadigital.io>
2022-05-27 19:10:06 +0300

    Initial commit

---
+++ src/main/kotlin/com/playzone/Application.kt
@@ -0,0 +1,11 @@
+package com.playzone
+
+import io.ktor.server.engine.*
+import io.ktor.server.cio.*
+import com.playzone.plugins.*
+
+fun main() {
+    embeddedServer(CIO, port = 8080, host = "0.0.0.0") {
+        configureRouting()
+    }.start(wait = true)
+}
